b19f98366adcd16ead8ffd43a393e540bb97810d
dahed92 <32330578+dahed92@users.noreply.github.com>
2019-01-15 18:49:58 +0100

    Bloc flow version 1
    Bloc sans integration de howler

---
+++ FLOW/platforms/android/app/src/main/assets/www/js/tab-2.js
@@ -0,0 +1,9 @@
+// var i = 0;
+// var original = document.getElementsByClassName('flow2')[0];
+// console.log(original);
+
+// function duplicates() {
+//     var clone = original.cloneNode(true); 
+//     clone.Class = "flow2" + ++i;
+//     original.parentNode.appendChild(clone);
+// }
